86164ee0669d2c98595008fa7e406c336069c89d
Tino Hager <tino.hager@nager.at>
2026-07-21 22:53:03 +0200

    Cleanup code

--- src/Nager.SmtpServerCore/Extensions/TaskExtensions.cs
+++ src/Nager.SmtpServerCore/Extensions/TaskExtensions.cs
@@ -12,27 +12,6 @@ namespace Nager.SmtpServerCore.Extensions
         /// <summary>
         /// Configures the task to stop waiting when the cancellation has been requested.
         /// </summary>
-        /// <param name="task">The task to wait for.</param>
-        /// <param name="cancellationToken">The cancellation token to watch.</param>
-        /// <returns>The original task.</returns>
-        public static async Task WithCancellation(this Task task, CancellationToken cancellationToken)
-        {
-            var tcs = new TaskCompletionSource<bool>();
-
-            using (cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).TrySetResult(true), tcs))
-            {
-                if (task != await Task.WhenAny(task, tcs.Task).ConfigureAwait(false))
-                {
-                    throw new OperationCanceledException(cancellationToken);
-                }
-            }
-
-            await task.ConfigureAwait(false);
-        }
-
-        /// <summary>
-        /// Configures the task to stop waiting when the cancellation has been requested.
-        /// </summary>
         /// <typeparam name="T">The return type of the task.</typeparam>
         /// <param name="task">The task to wait for.</param>
         /// <param name="cancellationToken">The cancellation token to watch.</param>
